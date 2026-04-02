5dc9acad822df6bb3ed1b80bed0d4fadb75d4c17
KNikhith <nikhithkotluri@gmail.com>
2026-04-02 12:08:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+let output=document.getElementById("output");
+
+let startPromise=new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve([1,2,3,4]);
+	},3000)
+});
+startPromise.then((arr)=>{
+	return new Promise((resolve,reject)=>{
+		let evenArr=arr.filter(x=>x%2==0);
+		setTimeout(()=>{
+			output.textContent=evenArr.toString();
+			resolve(evenArr);
+		},1000);
+	});
+})
+.then((evenArr)=>{
+	return new Promise((resolve,reject)=>{
+		let double=evenArr.map(x=>x*2);
+		setTimeout(()=>{
+			output.textContent=double.toString();
+		},2000)
+	})
+});
+.catch((err)=>console.log(err));
